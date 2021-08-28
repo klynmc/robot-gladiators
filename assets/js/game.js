1cab8f145f6c3e96ce922f3b46985beffe8b25e9
Leia <87383466+klynmc@users.noreply.github.com>
2021-08-28 12:51:29 -0500

    fight-skip loop

--- assets/js/game.js
+++ assets/js/game.js
@@ -5,14 +5,19 @@ var randomNumber = function(min, max) {
   return value;
 };
 
-// fight function (now with parameter for enemy's name)
-var fight = function(enemy) {
-  while (playerInfo.health > 0 && enemy.health > 0) {
-    // ask player if they'd like to fight or run
-    var promptFight = window.prompt('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
+var fightOrSkip = function() {
+  //ask player if they'd like to fight or skip using fightOrSkip function
+  var promptFight = window.prompt('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
+
+    // conditional recursive function call 
+    if (promptFight === "" || promptFight === null) {
+      window.alert("You need to provide a valid answer! Please try again.");
+      return fightOrSkip();
+    }
 
     // if player picks "skip" confirm and then stop the loop
-    if (promptFight === 'skip' || promptFight === 'SKIP') {
+    promptFight = promptFight.toLowerCase();
+    if (promptFight === "skip") {
       // confirm player wants to skip
       var confirmSkip = window.confirm("Are you sure you'd like to quit?");
 
@@ -21,14 +26,19 @@ var fight = function(enemy) {
         window.alert(playerInfo.name + ' has decided to skip this fight. Goodbye!');
         // subtract money from playerInfo.money for skipping
         playerInfo.money = Math.max(0, playerInfo.money - 10);
-        console.log("playerInfo.money", playerInfo.money)
-        break;
-      }
+        
+        return true;
     }
-
-    // remove enemy's health by subtracting the amount set in the playerInfo.attack variable
-    // generate random damage value based on player's attack power
-    var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
+    return false;
+}
+// fight function (now with parameter for enemy's name)
+var fight = function(enemy) {
+  while (playerInfo.health > 0 && enemy.health > 0) {
+    if (fightOrSkip()) {
+      // if true, leave fight by breaking loop
+      break;
+    }
+    } 
 
     enemy.health = Math.max(0, enemy.health - damage);
     console.log(
